6063b654a563708eb3a6016a8b999dc48f316642
parshad11 <ghimire.parshad11@gmail.com>
2021-04-30 21:45:39 +0545

    banner api

---
+++ app/Http/Controllers/Api/BannerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+Use App\Models\Banner;
+use Illuminate\Support\Facades\DB;
+
+class BannerController extends Controller
+{
+    //
+    public function banner()
+    {
+        $path = asset('/storage/banners/images/');
+        $banner = Banner::where('status', 1)
+            ->select(
+                'banners.id',
+                DB::raw("CONCAT('$path',banners.image) as banner_image")
+            )
+            ->get();
+        return response()->json([
+            'data' => [
+                'banner' => $banner,
+            ]
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,4 @@ Route::namespace('Api')->group(function () {
     Route::post('/login', 'AuthController@login');
     Route::post('/register', 'AuthController@register');
 });
+Route::get('/banners', 'Api\BannerController@banner');
